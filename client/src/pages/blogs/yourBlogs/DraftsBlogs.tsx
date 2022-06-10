b8c0e55e80e3018b4942ca5c95aae6eb7ae2e441
Khang <doduckhang298@gmail.com>
2022-06-10 17:28:45 +0700

    update draft blog to User

--- client/src/pages/blogs/yourBlogs/DraftsBlogs.tsx
+++ client/src/pages/blogs/yourBlogs/DraftsBlogs.tsx
@@ -1,16 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import NotFound from "../../../components/global/NotFound";
+import NotValue from "../../../components/global/NotValue";
 import blogAction from "../../../redux/action/blogAction";
 import {
   authSelector,
   draftBlogSelector,
 } from "../../../redux/selector/selectors";
+import { IBlog, IUser } from "../../../utils/Typescript";
 import CardDraftBlog from "../Card/CardDraftBlog";
 
 const DraftsBlogs = () => {
   const { authUser } = useSelector(authSelector);
   const { draftBlogs } = useSelector(draftBlogSelector);
+
+  const [blog, setBlog] = useState<IBlog>();
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -18,16 +22,27 @@ const DraftsBlogs = () => {
     blogAction.getDraftBlogs(authUser.access_token, dispatch);
   }, [dispatch, authUser.access_token]);
 
+  useEffect(() => {
+    const blog = draftBlogs.find(
+      (item) => (item.user as IUser)._id === authUser.user?._id
+    );
+    setBlog(blog);
+  }, [authUser.user?._id, draftBlogs]);
+
   if (!authUser.access_token) return <NotFound />;
   return (
     <div className="flex col-span-2 gap-2">
-      {draftBlogs.map((item, index) => {
-        return (
-          <div className="" key={index}>
-            <CardDraftBlog blog={item} />
-          </div>
-        );
-      })}
+      {blog ? (
+        draftBlogs.map((item, index) => {
+          return (
+            <div className="" key={index}>
+              <CardDraftBlog blog={item} />
+            </div>
+          );
+        })
+      ) : (
+        <NotValue />
+      )}
     </div>
   );
 };
